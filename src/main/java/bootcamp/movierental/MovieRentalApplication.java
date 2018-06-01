1da1136a75f41aea1e5dea1b9906f4798bcd81da
Abhinav Manchanda <abhinav.manchanda@gmail.com>
2018-06-01 13:05:43 +0530

    Movie Rental App now returns the list of movies avaialable.

--- src/main/java/bootcamp/movierental/MovieRentalApplication.java
+++ src/main/java/bootcamp/movierental/MovieRentalApplication.java
@@ -1,4 +1,12 @@
 package bootcamp.movierental;
 
+import java.util.Arrays;
+import java.util.List;
+
 public class MovieRentalApplication {
+    public List<String> list() {
+        return Arrays.asList("The Shawshank Redemption", "The Godfather",
+                "The Godfather : Part II", "The Dark Knight",
+                "12 Angry Men", "Schindler's List", "Pulp Fiction");
+    }
 }
--- src/test/java/bootcamp/movierental/MovieRentalApplicationTest.java
+++ src/test/java/bootcamp/movierental/MovieRentalApplicationTest.java
@@ -2,11 +2,24 @@ package bootcamp.movierental;
 
 import org.junit.Test;
 
+import java.util.Arrays;
+import java.util.List;
+
+import static junit.framework.TestCase.assertEquals;
 import static org.junit.Assert.*;
 
 public class MovieRentalApplicationTest {
 
     @Test
-    public void dummy(){}
+    public void viewTheListOfMovieNamesAvailable(){
+        List<String> expectedMovies = Arrays.asList("The Shawshank Redemption", "The Godfather",
+                "The Godfather : Part II", "The Dark Knight",
+                "12 Angry Men", "Schindler's List", "Pulp Fiction");
+        MovieRentalApplication movieRentalApplication = new MovieRentalApplication();
+        List<String> actualMovies = movieRentalApplication.list();
+        assertEquals(7, actualMovies.size());
+        assertEquals(expectedMovies, actualMovies);
+
+    }
 
 }
